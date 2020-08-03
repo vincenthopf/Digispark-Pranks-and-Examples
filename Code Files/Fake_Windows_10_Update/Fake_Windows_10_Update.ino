8b11a24c501f4edf8e2be2fc3b99fa82b8e96b44
Vincent Hopf - Jidi Parts <51283084+Jidiparts@users.noreply.github.com>
2020-08-03 16:54:53 +1000

    Update Fake_Windows_10_Update.ino

--- Code Files/Fake_Windows_10_Update/Fake_Windows_10_Update.ino
+++ Code Files/Fake_Windows_10_Update/Fake_Windows_10_Update.ino
@@ -9,12 +9,11 @@ int randNumber;
 const int TIMEDELAY = 2000;           // The delay before allowing the program to loop again
 
 void setup() {
-  DigiKeyboard.delay(300000); //Wait Five Minutes to prevent the device from interfering with user login
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  DigiKeyboard.print("https://fakeupdate.net/win10ue/");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(3000);
+  DigiKeyboard.delay(200);
   DigiKeyboard.sendKeyStroke(KEY_F11);
 }
   
