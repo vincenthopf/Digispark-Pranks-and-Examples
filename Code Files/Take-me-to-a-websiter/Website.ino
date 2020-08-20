54eeac3929ea144b6f6c0c14bae7b8555642dcb7
Vincent Hopf <51283084+Vincents-3d@users.noreply.github.com>
2020-08-20 21:20:09 +1000

    Update Website.ino

--- Code Files/Take-me-to-a-websiter/Website.ino
+++ Code Files/Take-me-to-a-websiter/Website.ino
@@ -1,17 +1,18 @@
+// This Code is written by Vincent J Hopf - www.vjs3d.cc
+// You will need an internet connection and a web browser for this code to work.
 #include "DigiKeyboard.h"//Include the library "DigiKeyboard.h"
 int i = 1;
-void setup() {
-  DigiKeyboard.delay(200);//Pause for 200 milliseconds
-  DigiKeyboard.sendKeyStroke(0);
-  }
+void setup() { //runs once on startup.
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(0);// This isn't necessary but it stops any unintended keys being pressed.
+}
 
 void loop() {  
   delay(500);//Pause for 500 milliseconds
   while(i==1){
     DigiKeyboard.delay(600);//Pause for 600 milliseconds
     DigiKeyboard.print("https://jidiparts.wordpress.com/");//Types out the web address
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);//press the ENTER key
-    i = 2;//stops the program from running multiple times 
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);//presses the ENTER key.
+    i = 2;//stops the program from running multiple times.
   }  
 }
-//Website.ino
